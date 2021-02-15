ae1e2480e7efd55b66facc0928e4384a2a93dcca
lvkoppen <lennart.vankoppen@tellick.nl>
2021-02-15 17:45:17 +0100

    Not working yet

---
+++ Opdracht1/F/ganglamp.js
@@ -0,0 +1,83 @@
+
+const chalk = require('chalk');
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+
+
+client.on('connect', function () {
+    client.subscribe('gebouw/lampen', function (err) {
+      if (client.connected==true) {
+        console.log('connected to topic gebouw/lampen')
+      }
+    })
+    client.subscribe('gebouw/beweging', function (err) {
+      if (client.connected==true) {
+        console.log('connected to topic gebouw/beweging')
+      }
+    })
+  })
+
+
+
+
+//var helderheid =process.env['Helderheid'];
+var lampsettings = { 
+  "interval" : 2000,
+  "status": 1,
+  "rgb":{
+      "r":255,
+      "g":255,
+      "b":255
+  }
+}
+
+console.log(chalk.rgb(lampsettings.rgb.r, lampsettings.rgb.g, lampsettings.rgb.b).underline('This is a simulated dimmable bulb'));
+
+lampsettings.status = 0
+print();
+lampsettings.rgb.r = 128;
+lampsettings.rgb.g = 128;
+lampsettings.rgb.b = 128;
+
+
+
+setInterval(print, 2000);
+
+function print() {
+	//console.clear();
+	if (lampsettings.status == 0) {
+        console.log(chalk.rgb(0, 0, 0).underline('Light is switched off'));
+	}
+	else {
+		console.log(chalk.rgb(lampsettings.rgb.r, lampsettings.rgb.g, lampsettings.rbg.b)('This is a simulated bulb'));
+	}
+}
+
+function updatesettings(settings){
+  lampsettings = settings;
+}
+
+function changestatus(state){
+  lampsettings.status = state;
+}
+function beweging(){
+  changestatus(1)
+  setTimeout(changestatus, lampsettings.interval, 0)
+
+}
+
+client.on('message', function (topic, message) {
+      switch(topic){
+        case 'gebouw/lampen':
+          updatesettings(JSON.parse(message.toString()));
+          break;
+        case 'gebouw/beweging':
+          beweging();
+          break;
+        default:
+          console.log("geen juiste topic gevonden");
+          break;
+
+      }
+});
---
+++ Opdracht1/F/updatelamp.js
@@ -0,0 +1,36 @@
+const readline = require('readline');
+const fs = require('fs');
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+
+
+client.on('connect', function () {
+    client.subscribe('gebouw/lampen', function (err) {
+      if (client.connected==true) {
+        console.log('connected')
+      }
+    })
+  })
+
+
+var lampsetting;
+
+fs.readFile('C:\\Users\\Lennart\\Desktop\\IoT1-Practica\\IOT1-Practica\\Opdracht1\\F\\lampsettings.json', (err, data) => {
+    if (err) throw err;
+    lampsetting = JSON.stringify(JSON.parse(data));
+});
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Press enter voor updaten");
+
+
+rl.on('line', (input) => {
+  console.log("sterkte ingevoerd");
+  client.publish('gebouw/lampen', lampsetting)
+});
