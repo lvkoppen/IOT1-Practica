aea4545abbc06f3120334de167368ecbfd0e3a15
lvkoppen <lennart.vankoppen@tellick.nl>
2021-02-15 22:43:59 +0100

    Added G

---
+++ Opdracht1/G/bewegingsensor.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+
+
+client.on('connect', function () {
+    client.subscribe('gebouw/lampen', function (err) {
+      if (client.connected==true) {
+        console.log('connected')
+      }
+      else{
+          console.log('not connected')
+      }
+    })
+  })
+
+var settings = {status: 1}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Press enter voor beweging");
+
+rl.on('line', (input) => {
+  console.log("Er is beweging geconstateerd");
+  client.publish('gebouw/lampen', JSON.stringify(settings))
+});
---
+++ Opdracht1/G/ganglamp.js
@@ -0,0 +1,77 @@
+
+const chalk = require('chalk');
+
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt://broker.mqttdashboard.com')
+
+
+client.on('connect', function () {
+    client.subscribe('gebouw/lampen', function (err) {
+      if (client.connected==true) {
+        console.log('connected to topic gebouw/lampen')
+      }
+    })
+  })
+
+//var helderheid =process.env['Helderheid'];
+var lampsettings = { 
+  "interval" : 2000,
+  "status": 1,
+  "rgb":{
+      "r":255,
+      "g":255,
+      "b":255
+  }
+}
+
+console.log(chalk.rgb(lampsettings.rgb.r, lampsettings.rgb.g, lampsettings.rgb.b).underline('This is a simulated dimmable bulb'));
+
+lampsettings.status = 0
+print();
+lampsettings.rgb.r = 128;
+lampsettings.rgb.g = 128;
+lampsettings.rgb.b = 128;
+
+
+
+setInterval(print, 2000);
+
+function print() {
+	//console.clear();
+	if (lampsettings.status == 0) {
+        console.log(chalk.rgb(0, 0, 0).underline('Light is switched off'));
+	}
+	else {
+		console.log(chalk.rgb(lampsettings.rgb.r, lampsettings.rgb.g, lampsettings.rgb.b)('This is a simulated bulb'));
+	}
+}
+
+function updatesettings(settings){
+  
+  if(typeof settings !== 'undefined' && settings){
+      Object.keys(settings).forEach(function(key){
+        lampsettings[key] = settings[key]
+      })
+      if(lampsettings.status ==1){
+        beweging(lampsettings)
+      }
+  }  
+}
+function changestatus(state){
+  lampsettings.status = state
+}
+function beweging(){
+  setTimeout(changestatus, lampsettings.interval, 0)
+}
+
+client.on('message', function (topic, message) {
+      switch(topic){
+        case 'gebouw/lampen':
+          updatesettings(JSON.parse(message.toString()));
+          break;
+        default:
+          console.log("geen juiste topic gevonden");
+          break;
+
+      }
+});
